29363036d9e37d0c6297f935e4010ddc8004c905
MsQuestGO <mrquestgo@gmail.com>
2026-06-07 15:07:18 +0200

    Change base path in vite.config.js to root

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  base: '/Portfolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
